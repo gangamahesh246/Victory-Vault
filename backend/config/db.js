c52a57807b36a6f699560640a9530d64e1d79870
gangamahesh246 <gangamahesh809@gmail.com>
2025-02-28 19:56:53 +0530

    'vercel'

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         const connection = await mongoose.connect(process.env.MONGO_URL, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         })
 
         console.log(`MongoDB connected: ${connection.connection.host}`)
