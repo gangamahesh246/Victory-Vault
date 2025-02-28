dbe2635a467898bff9a4b87b4d05606862a3351a
gangamahesh246 <gangamahesh809@gmail.com>
2025-02-28 19:43:24 +0530

    'vercel'

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const mongoose = require('mongoose');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const match = require('./routes/MathRoute');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const connectDB = require('./config/db');
 connectDB();
 
 const port = 7000;
 
-var app = express();
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/', match);
 
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.listen(port, () => {
-  console.log("server is running on port 7000 or 8000");
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log("server is running on port 7000");
 });
 
 module.exports = app;
